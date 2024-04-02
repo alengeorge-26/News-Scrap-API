bdd07afdd5473dfd5117fcd45ef1e9708695a245
alengeorge-26 <alengeorge261@gmail.com>
2024-04-02 22:23:21 +0530

    Commit

--- app.py
+++ app.py
@@ -1,37 +1,38 @@
-from flask import Flask,request
-from  flask_restful import Resource,Api
+from flask import Flask,request,jsonify
+from flask_restful import Resource,Api
 from newspaper import Article
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 api=Api(app)
 
-class newsText(Resource):
-    def get(self):
-        data=request.json
-        toi_article = Article(data['url'], language="en") 
+@app.route('/scrap',methods=['POST'])
+def scrap():
+    data=request.json
 
-        toi_article.download()
-        
-        toi_article.parse()
+    toi_article = Article(data['url'], language="en")
 
-        paragraphs = toi_article.text.split("\n")
-
-        chars_to_remove = ['/', '"']
-
-        cleaned_text_list = []
-        for string in paragraphs:
-            for char in chars_to_remove:
-                string = string.replace(char, "")
-            cleaned_text_list.append(string)
-
-        for string in cleaned_text_list:
-                if string=="":
-                    cleaned_text_list.remove(string)
-
-        return cleaned_text_list
+    toi_article.download()
     
-api.add_resource(newsText,'/')
+    toi_article.parse()
+
+    paragraphs = toi_article.text.split("\n")
+
+    chars_to_remove = ['/', '"']
+
+    cleaned_text_list = []
+    for string in paragraphs:
+        for char in chars_to_remove:
+            string = string.replace(char, "")
+        cleaned_text_list.append(string)
+
+    for string in cleaned_text_list:
+            if string=="":
+                cleaned_text_list.remove(string)
+
+    return jsonify(cleaned_text_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
