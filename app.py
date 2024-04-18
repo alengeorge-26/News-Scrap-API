98e93f8f784dc22e30bae90965a572e1feff23cc
alengeorge-26 <alengeorge261@gmail.com>
2024-04-18 23:28:54 +0530

    Changes

--- app.py
+++ app.py
@@ -10,31 +10,34 @@ api=Api(app)
 
 @app.route('/scrap',methods=['POST'])
 def scrap():
-    data=request.json
+    try:
+        data=request.json
 
-    toi_article = Article(data['url'], language="en")
+        toi_article = Article(data['url'], language="en")
 
-    toi_article.download()
-    
-    toi_article.parse()
+        toi_article.download()
+        
+        toi_article.parse()
 
-    paragraphs = toi_article.text.split("\n")
+        paragraphs = toi_article.text.split("\n")
 
-    chars_to_remove = ['/','"','(' ,')','|']
+        chars_to_remove = ['/','"','(' ,')','|']
 
-    cleaned_text_list = []
-    new_array = []
-    for string in paragraphs:
-        for char in chars_to_remove:
-            string = string.replace(char, "")
-        cleaned_text_list.append(string)
+        cleaned_text_list = []
+        news_array = []
+        for string in paragraphs:
+            for char in chars_to_remove:
+                string = string.replace(char, "")
+            cleaned_text_list.append(string)
 
-    for string in cleaned_text_list:
-        if string=="":
-            continue
-        new_array.append(string)
-
-    return jsonify(new_array)
+        for string in cleaned_text_list:
+            if string=="":
+                continue
+            news_array.append(string)
+            
+        return jsonify({'status':'true','news_array':news_array})
+    except:
+        return jsonify({'status':'false'})
 
 if __name__ == '__main__':
     app.run(debug=True)
